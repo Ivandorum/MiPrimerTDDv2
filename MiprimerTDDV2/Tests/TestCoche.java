9c4979a2f49418267b6628a08edbeb9e5f68d09c
ivana <ivana@Ord_Ivan>
2023-03-31 12:51:51 +0200

    Creado la rama Refactorizado y modificado los metodos

--- MiprimerTDDV2/Tests/TestCoche.java
+++ MiprimerTDDV2/Tests/TestCoche.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_crear_coche_Ivan() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	public void test_al_acelerar_un_coche_su_velocidad_aumenta_Ivan() {
 		Coche nuevoCoche = new Coche();
-		nuevoCoche.acelerar(30);
+		nuevoCoche.acelerar_Ivan(30);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	
 	@Test
-	public void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+	public void test_al_decelerar_un_coche_su_velocidad_disminuye_Ivan() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad=50;
-		nuevoCoche.decelerar(80);
+		nuevoCoche.decelerar_Ivan(80);
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	
--- MiprimerTDDV2/src/Coche.java
+++ MiprimerTDDV2/src/Coche.java
@@ -3,11 +3,12 @@ public class Coche {
 
 	public int velocidad;
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_Ivan(int aceleracion) {
 		velocidad+=aceleracion;
 	}
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_Ivan(int deceleracion) {
 		velocidad-=deceleracion;
+		if (velocidad<0)velocidad=0;
 	}
 }
