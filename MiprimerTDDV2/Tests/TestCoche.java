342bfb08cd2790018fff5ecf7aebca171f075f03
ivana <ivana@Ord_Ivan>
2023-03-31 12:10:28 +0200

    Primer commit

---
+++ MiprimerTDDV2/Tests/TestCoche.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestCoche {
+
+	@Test
+	public void test_crear_coche() {
+		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
+	}
+
+}
